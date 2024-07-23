77e592eb3cc387d9d6d01b6879f4e4bd618c4e86
LeoUrriste <urristeleonardo@gmail.com>
2024-07-23 18:47:54 -0300

    estructurada

---
+++ estructurada/EstructuraIf.java
@@ -0,0 +1,13 @@
+package estructurada;
+
+public class EstructuraIf {
+    public static void main(String[] args) {
+
+        int topPuntaje = 99;
+
+        if ( topPuntaje < 100 ){
+            System.out.println("El valor de puntaje es: " + topPuntaje);
+        }
+        System.out.println("fuera del if");
+    }
+}
