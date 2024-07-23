399e31d663e5b3c3c9a19eb260cad499a24afa07
LeoUrriste <urristeleonardo@gmail.com>
2024-07-23 19:57:11 -0300

    estructurada while

---
+++ estructurada/EstructutaDoWhile.java
@@ -0,0 +1,14 @@
+package estructurada;
+
+public class EstructutaDoWhile {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        do {
+            System.out.println("ok");
+            contador = contador - 1;
+        }while (contador > 0);
+
+    }
+}
