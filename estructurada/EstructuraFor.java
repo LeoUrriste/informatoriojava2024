7e48faaca1b1cb173f5e2cfd88e991e4aad72321
LeoUrriste <urristeleonardo@gmail.com>
2024-07-23 20:05:41 -0300

    estructurada for

---
+++ estructurada/EstructuraFor.java
@@ -0,0 +1,12 @@
+package estructurada;
+
+public class EstructuraFor {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++){
+            //declara - condicion - modifica
+            System.out.println("cualquier cosa");
+        }
+
+    }
+}
