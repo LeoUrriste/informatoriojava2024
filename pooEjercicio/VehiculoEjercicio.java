11c377755f710287a772e111aabed6eaa879b4d0
LeoUrriste <urristeleonardo@gmail.com>
2024-07-26 17:43:12 -0300

    ejercicio-avance

--- pooEjercicio/VehiculoEjercicio.java
+++ pooEjercicio/VehiculoEjercicio.java
@@ -5,12 +5,32 @@ public class VehiculoEjercicio {
     private String marca;
     private int cantidadDeRuedas;
 
+    protected boolean competencia;
+    protected boolean alarma;
 
 
     private String kmMaximo;
     private String kmMinimo;
 
 
+    //constructor
+    public VehiculoEjercicio(String marca, int cantidadDeRuedas, boolean competencia, String kmMaximo){
+        this.marca = marca;
+        this.cantidadDeRuedas = cantidadDeRuedas;
+        this.competencia = true;
+        this.alarma = true;
+        this.kmMaximo = kmMaximo;
+
+    }
+
+    public VehiculoEjercicio(String marca, int cantidadDeRuedas, boolean competencia, String kmMinimo, String kmMaximo){
+        this.marca = marca;
+        this.cantidadDeRuedas = cantidadDeRuedas;
+        this.competencia = true;
+        this.alarma = true;
+        this.kmMaximo = kmMaximo;
+        this.kmMinimo = kmMinimo;
+    }
 
 
     public String getMarca() {
@@ -44,4 +64,22 @@ public class VehiculoEjercicio {
     public void setKmMinimo(String kmMinimo) {
         this.kmMinimo = kmMinimo;
     }
+
+    public boolean isCompetencia() {
+        return competencia;
+    }
+
+    public void setCompetencia(boolean competencia) {
+        this.competencia = competencia;
+    }
+
+    public boolean isAlarma() {
+        return alarma;
+    }
+
+    public void setAlarma(boolean alarma) {
+        this.alarma = alarma;
+    }
+
+    
 }
